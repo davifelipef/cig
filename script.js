0504ebb40ce3dd3353a02c8fc6f87ac724e368b0
davifelipef <feliperedfield@gmail.com>
2023-06-16 19:16:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ window.onload = function(e){
   document.getElementById("defaultOpen").click();
   // Attach an event listener to the input element
   document.getElementById("dum_date").addEventListener("change", dateCalc);
+  document.getElementById("week_number").addEventListener("change", dateCalc);
+  document.getElementById("days_number").addEventListener("change", dateCalc);
   /* Calls the primigesta toggle funcion onload so it works right 
   out of the gate */
   primHandler();
@@ -31,9 +33,15 @@ function openTab(evt, tabName) {
     button that opened the tab */
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
+    // Resets all the values on tab change
     document.getElementById("dum_date").value = "";
     document.getElementById("dum_result").innerHTML = "Não calculada";
     document.getElementById("dpp_result").innerHTML = "Não calculada";
+    document.getElementById("ult_date").value = "";
+    document.getElementById("week_number").value = "";
+    document.getElementById("days_number").value = "";
+    document.getElementById("ult_result").innerHTML = "Não calculada";
+    document.getElementById("ult_dpp_result").innerHTML = "Não calculada";
 } 
 
 // Function that handles the primigesta information toggle button
@@ -64,6 +72,7 @@ function primHandler () {
 
 // Main calculation function
 function dateCalc() {
+  // This contains the DUM page calculations
   // gets the current date
   var today = new Date();
   // gets the date informed by the user
@@ -100,36 +109,18 @@ function dateCalc() {
     // Updates the screen DUM result
     document.getElementById("dum_result").innerHTML = dum_result;
   }
-  // This is the part where the DPP is calculated
+  // This is the part where the DPP is calculated based on the DUM
   // first a new date object is created
   var calcDate = new Date(userDate.getTime());
   calcDate.setDate(calcDate.getDate() + dpp_sum);
-  console.log("DUM + primigesta = " + calcDate);
   // Extract the month component to use in the next calculations
   var dum_month = calcDate.getMonth() + 1;
   // Checks what month it is
-  switch(dum_month) {
-    // when the month is bigger than march
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-    case 10:
-    case 11:
-    case 12:
-      // Subtract 3 months from the date and add a full year to it
-      calcDate.setMonth(calcDate.getMonth() - 3)
-      calcDate.setFullYear(calcDate.getFullYear() + 1);
-      break;
-      // when the month is march, february or january
-    case 1:
-    case 2:
-    case 3:
-      // Adds 9 months to the date
-      calcDate.setMonth(calcDate.getMonth() + 9);
-      break;
+  if (dum_month >= 4 && dum_month <= 12) {
+    calcDate.setMonth(calcDate.getMonth() - 3);
+    calcDate.setFullYear(calcDate.getFullYear() + 1);
+  } else if (dum_month >= 1 && dum_month <= 3) {
+    calcDate.setMonth(calcDate.getMonth() + 9);
   }
   // Extract date  and year components
   var dum_day = calcDate.getDate();
@@ -137,7 +128,9 @@ function dateCalc() {
   // updates the month after the calculations are complete
   dum_month = calcDate.getMonth() + 1;
   // formats the date as 'dd/mm/aaaa'
-  var formatted_dpp = (dum_day < 10 ? '0' : '') + dum_day + '/' + (dum_month < 10 ? '0' : '') + dum_month + '/' + dum_year;
+  var formatted_dpp = (dum_day < 10 ? '0' : '') + 
+  dum_day + '/' + (dum_month < 10 ? '0' : '') + 
+  dum_month + '/' + dum_year;
   // 
   if (formatted_dpp.includes("NaN")) {
     //do nothing
@@ -151,4 +144,92 @@ function dateCalc() {
       document.getElementById("dpp_result").innerHTML = formatted_dpp;
     }
   }
+  // End of the DUM page calculations
+  
+  // Start of the Ultrassom page calculations
+  var ult_week = parseInt(document.getElementById("week_number").value);
+  if (isNaN(ult_week)) {
+    ult_week = 0;
+  } else {
+    // do nothing
+  }
+  var ult_day = parseInt(document.getElementById("days_number").value);
+  if (isNaN(ult_day)) {
+    ult_day = 0;
+  } else {
+    // do nothing
+  }
+  // gets the Ultrassom date informed by the user
+  const ultInput = new Date(document.getElementById("ult_date").value);
+  var ultDate = new Date(ultInput);
+  // Calculate the difference in milliseconds between the two dates
+  var timeDifference = today.getTime() - ultDate.getTime();
+  // Convert the time difference to days
+  var differenceInDays = Math.floor(timeDifference / (1000 * 3600 * 24));
+  // Calculate the number of weeks and remaining days
+  var weekNumber = Math.floor(differenceInDays / 7);
+  var daysNumber = differenceInDays % 7;
+  //daysNumber -= 1;
+  // updates the values
+  weekNumber += ult_week;
+  daysNumber += ult_day;
+  // creates the dum result variable
+  var ult_result = '';
+  // formats the texts to be displayed on the screen as weeks and days
+  if (weekNumber !== 0) {
+    ult_result += `${weekNumber} ${weekNumber !== 1 ? 'semanas' : 'semana'}`;
+  }
+  if (daysNumber !== 0) {
+    if (ult_result !== '') {
+      ult_result += ' e ';
+    }
+    ult_result += `${daysNumber} ${daysNumber !== 1 ? 'dias' : 'dia'}`;
+  }
+  console.log("Ult result is: " + ult_result);
+  // Updates the screen text fields
+  if (ult_result.includes("NaN")) {
+    // do nothing
+    document.getElementById("ult_result").innerHTML = "Não calculada";
+  } else {
+    // Updates the screen DUM result
+    document.getElementById("ult_result").innerHTML = ult_result;
+  }
+  // End of the Ultrassom page calculations
+
+  // This is the part where the DPP is calculated based on the Ultrassom
+  // first a new date object is created
+  var calcDate = new Date(ultDate.getTime());
+  calcDate.setDate(calcDate.getDate() + dpp_sum + 15 - daysNumber - weekNumber * 7);
+  // Extract the month component to use in the next calculations
+  var ult_month = calcDate.getMonth() + 1;
+  // Checks what month it is
+  if (ult_month >= 4 && ult_month <= 12) {
+    calcDate.setMonth(calcDate.getMonth() - 3);
+    calcDate.setFullYear(calcDate.getFullYear() + 1);
+  } else if (ult_month >= 1 && ult_month <= 3) {
+    calcDate.setMonth(calcDate.getMonth() + 9);
+  }
+  // Extract date and year components
+  var ult_day = calcDate.getDate();
+  var ult_year = calcDate.getFullYear();
+  // updates the month after the calculations are complete
+  ult_month = calcDate.getMonth() + 1;
+  // formats the date as 'dd/mm/aaaa'
+  var formatted_dpp = (ult_day < 10 ? '0' : '') + 
+    ult_day + '/' + (ult_month < 10 ? '0' : '') + 
+    ult_month + '/' + ult_year;
+  // 
+  if (formatted_dpp.includes("NaN")) {
+    // do nothing
+    document.getElementById("ult_dpp_result").innerHTML = "Não calculada";
+  } else {
+    if (formatted_dpp === "00/00/0000") {
+      // do nothing
+      document.getElementById("ult_dpp_result").innerHTML = "Não calculada";
+    } else {
+      // Updates the screen DPP result
+      document.getElementById("ult_dpp_result").innerHTML = formatted_dpp;
+    }
+  }
+  // End of the Ultrassom page calculations
 }
